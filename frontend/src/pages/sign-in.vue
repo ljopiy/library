b96e89f0cc61a54d85c1a5637ba6c72c6dc0612e
husainova <ahusainova96@gmail.com>
2025-11-22 17:19:37 +0500

    change: sign-in2

--- frontend/src/pages/sign-in.vue
+++ frontend/src/pages/sign-in.vue
@@ -26,11 +26,77 @@
 import { NInput } from 'naive-ui';
 import { NButton } from 'naive-ui';
 import iconBtn from '@/components/ui/icon-btn.vue';
-import IconBtn from '@/components/ui/icon-btn.vue';
 </script>
 
 <style scoped>
 .page-wrapper {
-  background-color: ;
+  background-color: #F7742A;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+}
+
+form button {
+  border: none;
+  background-color: transparent;
+  color: #CACACA;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 31px 24px;
+}
+
+.header h2 {
+  color: white;
+  margin: 0;
+}
+
+.header button {
+  margin: 0;
+  padding: 0;
+
+}
+
+
+
+.login {
+  background-color: white;
+  border-radius: 5%;
+  padding: 24px;
+  border: none;
+  border-top: 2px solid white;
+  display: flex;
+  flex-direction: column;
+  gap: 65px;
+  padding-bottom: 20%;
+}
+
+h1 {
+  padding: 0;
+  margin: 0;
+}
+
+.hello {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+p {
+  padding: 0;
+  margin: 0;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
 }
 </style>
