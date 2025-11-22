47a0e146c6f0741655febfaaf3a2d4ba7bfde3f7
husainova <ahusainova96@gmail.com>
2025-11-22 15:28:48 +0500

    add: sign-in

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SidnIn from '@/views/sign-in.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/sign-in',
+      name: 'SignIn',
+      component: SignIn,
+    }
+  ],
 })
 
 export default router
