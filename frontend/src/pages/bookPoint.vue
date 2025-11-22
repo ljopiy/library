3ec263228ccc927d522777068442c41eb0025548
husainova <ahusainova96@gmail.com>
2025-11-23 02:42:46 +0500

    add: bookPoint

--- frontend/src/pages/bookPoint.vue
+++ frontend/src/pages/bookPoint.vue
@@ -2,6 +2,7 @@
   <div class="page-wrapper">
     <div class="header"><h1>BookPoint</h1></div>
     <div class="reki">
+
       <h4>Рекомендуем к прочтению</h4>
       <ul>
         <bookCard></bookCard>
@@ -9,7 +10,7 @@
       </ul>
     </div>
     <div class="coworking">
-      <h2>Наш коворкинг</h2>
+      <h4>Наш коворкинг</h4>
       <ul>
         <li>
           <img src="../../public/assets/coworking.svg"></img>
@@ -26,21 +27,32 @@
       </ul>
       <NButton type="primary">Забронировать</NButton>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
 import { NButton } from 'naive-ui';
 import bookCard from '@/components/shared/bookCard.vue';
+import Footer from '@/components/shared/footer.vue';
 </script>
 
 <style scoped>
+
+.reki {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: #F3F3F3;
+  border-radius: 20px;
+}
 ul {
   display: flex;
   overflow-x: scroll;
   scroll-snap-type: x mandatory;
   scrollbar-width: none;
-    -ms-overflow-style: none;
+  -ms-overflow-style: none;
+
 }
 
 li {
@@ -51,6 +63,31 @@ li {
 
 li img {
   width: 100vw;
+}
+
+.page-wrapper{
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+
+h1 {
+  width: 100%;
+  text-align: center;
+}
+
+
+h4 {
+  padding: 10px 0 0 10px;
+}
+
+.coworking {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: #F3F3F3;
+  border-radius: 20px;
+  padding: 10px;
 }
 
 </style>
