51f42836d66e02e964c855d32ca5a5bb8b805f3b
anton787 <antongagelganc6443@gmail.com>
2025-11-22 16:21:30 +0500

    icon-btn

---
+++ frontend/src/components/ui/icon-btn.vue
@@ -0,0 +1,29 @@
+<template>
+  <button><slot>a</slot></button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+button{
+  all: unset;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  transition: all 0.3s ease;
+  border-radius: 100%;
+  min-height: 30px;
+  min-width: 30px;
+}
+
+button:hover {
+  background-color: rgba(186, 186, 186, 0.568);
+}
+
+button:active {
+  background-color: rgb(190, 190, 190);
+}
+</style>
