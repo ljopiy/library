a30d240b884a9279a401dede6e059fedd365033e
anton787 <antongagelganc6443@gmail.com>
2025-11-23 00:58:33 +0500

    init backend

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,33 +1,18 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-export const baseUrl = 'http://127.0.0.1:5000/'
+export const baseUrl = 'http://127.0.0.1:8000/'
 
 export const useDataStore = defineStore('data', {
   state: () => ({
     auth_key: '',
     role: '',
-    excursions: [],
-    residentExcursions: [],
-    excursionDetail: [],
-    profileData: [],
-    reservationsData: [],
+    books: [],
   }),
   actions: {
-    setTokenRole(auth_key, role) {
-      this.auth_key = auth_key
-      this.role = role
-    },
-    clearTokenRole() {
-      this.auth_key = ''
-      this.role = ''
-    },
-    deletEvent() {
-      this.residentExcursions = []
-    },
     async PostNewUser(jsonData) {
       try {
-        const response = await axios.post(`${baseUrl}/api/user/register`, jsonData, {
+        const response = await axios.post(`${baseUrl}/api/auth/login`, jsonData, {
           headers: {
             'Content-Type': 'application/json',
           },
@@ -40,10 +25,6 @@ export const useDataStore = defineStore('data', {
     },
   },
   getters: {
-    getProfileData: (state) => state.profileData,
-    getExcursions: (state) => state.excursions,
-    getExcursionDetail: (state) => state.excursionDetail,
-    getResidentEvents: (state) => state.residentExcursions,
   },
   persist: {
     key: 'data-store',
