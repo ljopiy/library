0ec53a4922cdc133167ec2deb34ec695935bc2b9
husainova <ahusainova96@gmail.com>
2025-11-22 15:50:10 +0500

    change: sign-in

--- frontend/src/pages/sign-in.vue
+++ frontend/src/pages/sign-in.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="page-wrapper">
-    <header></header>
+    <div class="header">
+      <button></button>
+      <h2>Авторизация</h2>
+    </div>
+    
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
